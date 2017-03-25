52bcb4f9b413344520254003c6d7c88be1943546
liufang <liufangzka@hotmail.com>
2017-03-25 18:04:10 -0500

    fd

--- process_control_block.h
+++ process_control_block.h
@@ -25,5 +25,6 @@ struct process_control_block{
     int is_waiting_to_write_to_terminal;
 };
 
+struct process_control_block *create_new_process(int pid, int parent_id);
 struct process_control_block * create_idle_process();
 struct process_control_block* create_empty_process(int pid, int parent_pid);
