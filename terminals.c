fff17cac81af123af9b27703f2fe0ca6c9d95290
liufang <liufangzka@hotmail.com>
2017-03-25 14:26:06 -0500

    fadf

--- terminals.c
+++ terminals.c
@@ -1,6 +1,10 @@
 //
 // Created by Liu Fang on 3/19/17.
 //
+#include "terminals.h"
+#include "process_scheduling.h"
+#include "process_control_block.h"
+#include "trap_handlers.h"
 
 void init_charbuffers(){
     charbuffers = malloc(sizeof(struct charbuffer)* NUM_TERMINALS);
--- terminals.h
+++ terminals.h
@@ -2,6 +2,8 @@
 #include <comp421/yalnix.h>
 #include <stdlib.h>
 
+struct charbuffer *charbuffers;
+
 struct charbuffer {
     char buffer[TERMINAL_MAX_LINE];
     int read;
