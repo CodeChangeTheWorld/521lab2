394e72948a673454f52f17e2457165efcac519da
liufang <liufangzka@hotmail.com>
2017-03-31 12:46:28 -0500

    fd

--- test_tty_read.c
+++ test_tty_read.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 
 int main(){
-    prinft("Begin tty read\n");
+    printf("Begin tty read\n");
     char *readbuffer = malloc(sizeof(char)* 10);
     TtyRead(0,readbuffer,5);
     printf("%s\n", readbuffer);
