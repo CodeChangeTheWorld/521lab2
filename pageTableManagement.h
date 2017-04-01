684b29a0adbc8c1efcccffa6a920d48316d2d04b
liufang <liufangzka@hotmail.com>
2017-03-31 19:21:50 -0500

    lkjk

--- pageTableManagement.h
+++ pageTableManagement.h
@@ -13,8 +13,7 @@ extern struct pte *kernel_page_table;
 
 void init_kernel_page_table();
 void add_first_record();
-void init_page_table(struct pte* page_table);
-void init_initial_page_table(struct pte* page_table);
+void init_page_table(struct pte* page_table,bool initial);
 struct pte *create_page_table();
 void free_page_table(struct pte *page_table);
 int num_pages_in_use(struct pte* page_table);
